cef625da966efd2abf33d9e6a1c1b4723bad7fe7
NeKroFR <70316327+NeKroFR@users.noreply.github.com>
2025-06-07 18:03:34 +0200

    better test2

--- tests/test2.py
+++ tests/test2.py
@@ -1,6 +1,8 @@
 
-def get_sol(name: str, first_name: bytes)-> str:
-    first_name_str = first_name.decode('utf-8')
-    return name + " " + first_name_str
+def get_sol(name: str, key: bytes) -> str:
+    name_bytes = bytearray(name.encode('utf-8'))
+    for i in range(len(name_bytes)):
+        name_bytes[i] ^= key[i % len(key)]
+    return name_bytes.decode('utf-8')
 
-print("Hello, my name is", get_sol("John", b"Doe") + ".")
+print("Hello, my name is", get_sol("xwethgcx", b'2\x18\r\x1aH#\x0c\x1d') + ".")
